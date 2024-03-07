aa5a405c829dbe1605c2223f49009a292badb9e4
marcius.ljunior <marcius.ljunior@CAR0626450W10-1>
2024-03-07 19:46:14 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    internal static bool run = false;
+    static void Main(string[] args)
+    {
+        // Criando uma nova thread e passando o método que ela executará
+        Thread thread = new Thread(new ThreadStart(ExecutarTarefa));
+
+        // Iniciando a execução da thread
+        thread.Start();
+
+        run = true;
+        while (run)
+        {
+            Console.WriteLine("Escreva yes para continuar, no para parar: ");
+            string action =  Console.ReadLine();
+            if(action == "no")
+            {
+                run = false;
+            }
+        }
+
+        // Espera a thread terminar antes de encerrar o programa
+        thread.Join();
+
+        Console.WriteLine("Programa principal terminou.");
+    }
+
+    static void ExecutarTarefa()
+    {
+        int i = 0;
+        while(run)
+        {
+            Console.WriteLine("Thread executando... " + i);
+            Thread.Sleep(1000); // Espera 1 segundo antes de imprimir a próxima mensagem
+            i++;
+        }
+    }
+}
