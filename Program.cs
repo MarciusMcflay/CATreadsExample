9f8b53e1292f711d7d684c420a94126e0a19b207
MarciusMcflay <jhorge.marcius@gmail.com>
2024-03-07 20:20:47 -0300

    mudado

--- Program.cs
+++ Program.cs
@@ -34,7 +34,7 @@ class Program
         int i = 0;
         while(run)
         {
-            Console.WriteLine("MarciusMcflay executando... " + i);
+            Console.WriteLine("MarciusMcfl executando... " + i);
             Thread.Sleep(1000); // Espera 1 segundo antes de imprimir a próxima mensagem
             i++;
         }
